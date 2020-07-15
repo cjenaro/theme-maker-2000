972c04fd50b51ff9f9680a8a1aafac551b7e9ed5
Jenaro Calvino <jen.calvineo@gmail.com>
2020-07-14 22:06:31 -0300

    Theme Maker 2000 initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const app = document.getElementById('app')
+app.innerHTML = "<h1>Theme Maker 2000!</h1>"
